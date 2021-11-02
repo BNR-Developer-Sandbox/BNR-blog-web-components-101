1f2f3829f3dc938a2640f553928892e4bee5c1f2
Loren <loren138@users.noreply.github.com>
2021-11-02 12:49:45 -0400

    lunch update

--- components/photo-gallery.js
+++ components/photo-gallery.js
@@ -8,6 +8,7 @@ customElements.define(
 
     async connectedCallback() {
       console.log("photo-gallery connectedCallback initHandler", this?.initHandler);
+        setTimeout(() => console.log("photo-gallery timeout initHandler", this?.initHandler), 0);
     }
 
     // TODO click on current image to open lightbox
