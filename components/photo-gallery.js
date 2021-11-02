764f950e0925e8bf95bf8d8868f281ef82b0d1bd
Ashley Parker <aparker@bignerdranch.com>
2021-11-02 15:31:31 -0400

    Adding photo component

--- components/photo-gallery.js
+++ components/photo-gallery.js
@@ -1,17 +1,35 @@
+import "./photo.js";
+
 customElements.define(
-  'wc-photo-gallery',
+  "wc-photo-gallery",
   class extends HTMLElement {
     constructor() {
       super();
+      this.attachShadow({ mode: "open" });
     }
 
     async connectedCallback() {
-      this.connected();
-      this.innerHTML = `
+      await this.connected();
+      this.render();
+      // console.log("js", this.images);
+    }
+
+    render() {
+      this.shadowRoot.innerHTML = `
         <ol>
-          <li>prev</li>
-          <li>current</li>
-          <li>next</li>
+          ${this.images
+            .slice(0, 1)
+            .map((image) => {
+              console.log("map start");
+              const photo = document.createElement("wc-photo");
+              photo.image = image;
+              photo.onclick = () => console.log("click handler");
+              photo.onprev = () => console.log("prev");
+              photo.onnext = () => console.log("next");
+              console.log("map end");
+              return photo.outerHTML;
+            })
+            .join("")}
         </ol>
       `;
     }
---
+++ components/photo.js
@@ -0,0 +1,57 @@
+customElements.define(
+  "wc-photo",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this._image = "";
+    }
+
+    set image(str) {
+      this._image = str;
+    }
+
+    get image() {
+      return this._image;
+    }
+
+    async connectedCallback() {
+      console.log("image", this.image);
+      let startTouchX = null;
+
+      this.ontouchstart = (e) => (startTouchX = e.touches[0].clientX);
+      this.ontouchend = (e) => {
+        if (Math.abs(startTouchX - e.changedTouches[0].clientX) > 25) {
+          if (startTouchX < e.changedTouches[0].clientX) {
+            this.onprev();
+          } else {
+            this.onnext();
+          }
+        }
+        startTouchX = null;
+      };
+      this.render();
+    }
+
+    render() {
+      this.shadowRoot.innerHTML = `
+        <style>
+          li {
+            list-style: none;
+            display: flex;
+            flex: 1;
+            max-width: 100vw;
+          }
+          li img {
+              width: 100%;
+          }
+        </style>
+          <li>
+            <img src="${this.image}"/>
+          </li>
+      `;
+    }
+
+    // TODO click on current image to open lightbox
+  }
+);
